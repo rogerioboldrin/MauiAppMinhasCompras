91413da46e967f281c962ed6fec1ad50952cbcf4
rogerioboldrin <rogerioboldrin@gmail.com>
2025-03-03 11:16:31 -0300

    Construção da classe para fazer CRUD no SQLite

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -8,7 +8,6 @@ namespace MauiAppMinhasCompras.Models
         public int Id { get; set; }
         public string Descricao { get; set; }
         public double Quantidade { get; set; }
-
         public double Preco {  get; set; } 
     }
 }
